6afe126934193d88c4d4b74ecf382c3762fd5db1
Diter Terrones <diter@Diters-MacBook-Air.local>
2020-04-12 17:22:44 -0500

    Basic Setup

---
+++ src/plugins/axios.js
@@ -0,0 +1,59 @@
+'use strict'
+
+import Vue from 'vue'
+import axios from 'axios'
+
+// Full config:  https://github.com/axios/axios#request-config
+// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+let config = {
+    // baseURL: process.env.baseURL || process.env.apiUrl || ""
+}
+
+export const httpClient = axios.create(config)
+
+httpClient.interceptors.request.use(
+    function(config) {
+        // Do something before request is sent
+        return config
+    },
+    function(error) {
+        // Do something with request error
+        return Promise.reject(error)
+    }
+)
+
+// Add a response interceptor
+httpClient.interceptors.response.use(
+    function(response) {
+        // Do something with response data
+        return response
+    },
+    function(error) {
+        // Do something with response error
+        return Promise.reject(error)
+    }
+)
+
+Plugin.install = function(Vue) {
+    Vue.axios = httpClient
+    window.axios = httpClient
+    Object.defineProperties(Vue.prototype, {
+        axios: {
+            get() {
+                return httpClient
+            }
+        },
+        $axios: {
+            get() {
+                return httpClient
+            }
+        }
+    })
+}
+
+Vue.use(Plugin)
+
+export default Plugin
